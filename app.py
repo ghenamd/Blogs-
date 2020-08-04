ba17df8cb52d2c3a273ba39a9bed837998bd30c8
Ghenadie Nit <evaflorn@yahoo.com>
2020-08-04 20:37:39 +0100

    Db init fix

--- app.py
+++ app.py
@@ -8,25 +8,19 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.story import Story
 
-from db import db
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '123456'
 api = Api(app)
 
+# Endpoints
 jwt = JWT(app, authenticate, identity)  # /auth
-
-
-@app.before_request
-def create_tables():
-    db.create_all()
-
-
 api.add_resource(UserRegister, '/register')
 api.add_resource(Story, '/blog/<string:username>')
 
 if __name__ == '__main__':
+    from db import db
+
     db.init_app(app)
     app.run(port=5002, debug=True)
